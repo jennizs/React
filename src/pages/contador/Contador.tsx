9877265d4828c8c2bb8cb704c9e88a2d772eac29
jennizs <jennyfrn6104@gmail.com>
2024-05-16 20:54:14 -0300

    implementando react

---
+++ src/pages/contador/Contador.tsx
@@ -0,0 +1,27 @@
+﻿import { useState } from "react"
+
+function Contador() {
+
+
+  const [valor, setvalor] = useState(0);
+
+function handleClick(){
+  setvalor(valor + 1);
+}
+
+// console.log("Variavel valor:" + valor);
+  return (
+    <div>
+      
+      <h2>Componente Contador</h2>
+
+      <p>O valor atual é: {valor} </p>
+
+      <button onClick={handleClick}>Adicionar 1</button>
+      
+      
+      </div>
+  )
+}
+
+export default Contador
